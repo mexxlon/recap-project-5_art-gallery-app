b4cb4efaad6aef70a52762ec2b64875a091a7bdf
Maximilian Hammes <maximview@gmail.com>
2024-08-15 16:55:59 +0200

    add styled components for spotlight

--- pages/spotlight.js
+++ pages/spotlight.js
@@ -1,4 +1,20 @@
+import styled from "styled-components";
 import Image from "next/image";
+
+const SpotlightContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const ArtistName = styled.h2`
+  background-color: #f4f4f4;
+  padding: 10px;
+  border-radius: 5px;
+  margin-top: 10px;
+  text-align: center;
+`;
 
 function randomSpotLight(pieces) {
   if (!pieces) return null;
@@ -11,15 +27,17 @@ export default function Spotlight({ pieces }) {
   if (!randomPiece) return null;
 
   return (
-    <div>
-      <h1>Spotlight</h1>
-      <Image
-        src={randomPiece.imageSource}
-        alt={randomPiece.name}
-        width={randomPiece.dimensions.width * 0.3}
-        height={randomPiece.dimensions.height * 0.3}
-      />
-      <h2>By {randomPiece.artist}</h2>
-    </div>
+    <SpotlightContainer>
+      <div>
+        <h1>Spotlight</h1>
+        <Image
+          src={randomPiece.imageSource}
+          alt={randomPiece.name}
+          width={randomPiece.dimensions.width * 0.3}
+          height={randomPiece.dimensions.height * 0.3}
+        />
+        <ArtistName>By {randomPiece.artist}</ArtistName>
+      </div>
+    </SpotlightContainer>
   );
 }
